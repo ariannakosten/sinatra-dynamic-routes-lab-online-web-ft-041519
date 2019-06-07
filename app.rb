f542279c0b85e64ea36edabdf6e855d2d9c1f42a
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-07 19:22:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-require_relative 'config/environment'class App < Sinatra::Base
+require_relative 'config/environment'
+class App < Sinatra::Base
   get '/reversename/:name' do
   @reverse_name = params[:name].reverse
   @reverse_name
